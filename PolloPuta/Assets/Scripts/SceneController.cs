b49817c36caf325789a9d7ee90b37fa925d0870b
MarkGyure <mgyure@mail.bradley.edu>
2024-12-08 13:27:03 -0600

    play button working

--- PolloPuta/Assets/Scripts/SceneController.cs
+++ PolloPuta/Assets/Scripts/SceneController.cs
@@ -3,15 +3,25 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    // This method will be called when the button is clicked to load the next scene
     public void LoadNextScene()
     {
+        // Get the current scene's index
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
 
+        // Calculate the next scene's index
         int nextSceneIndex = currentSceneIndex + 1;
 
+        // Check if the next scene exists in the build settings
         if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
         {
+            // Load the next scene
             SceneManager.LoadScene(nextSceneIndex);
+        }
+        else
+        {
+            // If there is no next scene, print a message
+            Debug.Log("No next scene available.");
         }
     }
 }
